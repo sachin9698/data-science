dd1137fb6cf578db9a4b726869da192edf8b95b4
sachin9698 <sachin.kumar9698@gmail.com>
2018-05-25 12:10:53 +0530

    modified

--- first.py
+++ first.py
@@ -1,5 +1,49 @@
 import numpy as np
 
 
-a=np.arange(20)
-print(a)
+#a=np.arange(20)
+#print(a)
+
+#np.random.seed(34)
+# b=np.random.rand(5)
+# b=np.random.rand(10,2,2) #(10=no of random no. ,2,2 = size of matrix)
+# print(b)
+
+# b=np.random.randn(5,2,2)
+# b=np.random.randint(2,10,8)  #(2,10 = range low and high , 8= no. of random no)
+# b=np.random.normal(10,3,11)
+# print(b)
+
+# np.random.seed(30)
+# dt=np.dtype(('string'))
+# a=np.random.randint(20,40,20)
+# a=np.array(a,dtype=dt)
+# print(a)
+
+# a=a.reshape(4,5)
+# print(a)
+# b=np.arange(10,20)
+# print(a.sum(0))  # 0 for coloumn and 1 for row
+# print(a[1,2])
+# print(a.max())
+# print(a.min())
+# print(a.argmax()) #return the index of max value
+# print(a.argmin()) #return the index of min value
+
+# for i in range(0,20):
+#     if(a[i]>=25 and a[i]<=30):
+#         print(a[i])
+
+# print(a[(a>=25) & (a<=30)])
+# bl=[0,1,3,7]
+# print(np.count_nonzero(a[a==25]))
+
+
+# s=input()
+# ms=''
+# for i in s:
+#     if i.lower() not in 'aeiou':
+#         ms+=i
+# print(ms)
+# a=[1,2,3,4]
+# print(a)
